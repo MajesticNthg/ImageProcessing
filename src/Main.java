a6c67bfd0c0399fad95e715e93613a42d89493b8
MajesticNthg <majestic_nthg@mail.ru>
2023-12-20 02:01:39 +1000

    mod.1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,60 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.*;
+
+
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception {
+        BufferedImage newImage1 = new BufferedImage(800,600, BufferedImage.TYPE_INT_RGB);
+        ImageIO.write(newImage1, "png", new File("inPNG.png")); // создание первого изображения
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        BufferedImage newImage2 = new BufferedImage(800,600,BufferedImage.TYPE_INT_RGB);
+        ImageIO.write(newImage2, "jpg", new File("inJPG.jpg")); // создание второго изображения
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        BufferedImage newImage3 = new BufferedImage(800,600, BufferedImage.TYPE_INT_RGB);
+        ImageIO.write(newImage3, "gif", new File("inGIF.gif")); // создание третьего изображения
+
+        BufferedImage newImage4 = new BufferedImage(800,600, BufferedImage.TYPE_INT_RGB);
+        ImageIO.write(newImage4, "png", new File("inPNG2.png")); // создание четвертого изображения
+
+        BufferedImage newImage5 = new BufferedImage(800,600,BufferedImage.TYPE_INT_RGB);
+        ImageIO.write(newImage5, "jpg", new File("inJPG2.jpg")); // создание пятого изображения
+
+
+
+
+        String type1 = ".png"; // первый параметр расширения
+        String type2 = ".jpg"; // второй параметр расширения
+        createImageConvert(type1, type2); // задание 4.1 + 4.2
+
+
+    }
+
+    public static void createImageConvert(String type1, String type2) throws Exception {
+        File[] files = new File(".").listFiles(); // список файлов и каталогов текущей директории
+        for (File f : files) {
+            if (f.isDirectory()) continue;
+            else {
+                String extension = f.getName().substring(f.getName().indexOf("."));
+                if ((extension.equals(type1))) {
+                    int lastIndex = f.getName().lastIndexOf(".");
+                    File new_f_name = new File(f.getName().substring(0, lastIndex) + type2);
+                    f.renameTo(new_f_name); // переименование файла
+
+                    BufferedImage img = ImageIO.read(new File(new_f_name.getName()));
+                    Graphics2D g = img.createGraphics();
+                    g.setColor(Color.BLUE);
+                    int w = 150;
+                    g.drawRect ( (img.getWidth()-w)/2, (img.getHeight()-w)/2, w, w );
+                    g.drawString("Hello,", (img.getWidth())/2 - (w/4),(img.getHeight())/2);
+                    g.drawString("World!", (img.getWidth())/2 - (w/4),(img.getHeight())/2 + (w/4));
+                    g.dispose();
+                    ImageIO.write(img, "jpg", new File(new_f_name.getName())); // построение квадрата с текстом внутри
+                }
+            }
         }
     }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,4 +1,43 @@
-import static org.junit.jupiter.api.Assertions.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import org.junit.jupiter.api.Assertions;
+import javax.imageio.ImageIO;
+
+
 class MainTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void createImageConvert() throws Exception {
+            int x = 0;
+            Main img = new Main();
+            BufferedImage newImage1 = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
+            ImageIO.write(newImage1, "png", new File("inPNG.png")); // создание первого изображения
+
+            BufferedImage newImage2 = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
+            ImageIO.write(newImage2, "jpg", new File("inJPG.jpg")); // создание второго изображения
+
+            BufferedImage newImage3 = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
+            ImageIO.write(newImage3, "gif", new File("inGIF.gif")); // создание третьего изображения
+
+            BufferedImage newImage4 = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
+            ImageIO.write(newImage4, "png", new File("inPNG2.png")); // создание четвертого изображения
+
+            BufferedImage newImage5 = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
+            ImageIO.write(newImage5, "jpg", new File("inJPG2.jpg")); // создание пятого изображения
+
+            BufferedImage newImage6 = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
+            ImageIO.write(newImage6, "png", new File("inPNG2.jpg")); // шестое изображение, которое дублирует по названию будущее отредактированное
+            img.createImageConvert(".png", ".jpg");
+            File[] files = new File("F:\\Программирование\\Intellij IDEA\\ImageProcessing").listFiles();
+            for (File f : files) {
+                if (f.isDirectory()) continue;
+                else {
+                    String extension = f.getName().substring(f.getName().indexOf("."));
+                    if (extension.equals(".png")) {
+                        x++;
+                    }
+                }
+            }
+            Assertions.assertTrue(x < 1); // есть необработанные изображения
+    }
 }
